f79c6fc96dec0f95c2032d863a24b8393e872481
Eden Lopes <eden.lopes.paradev@gmail.com>
2026-05-12 11:20:18 -0300

    atualizando-o-footer

--- react/src/components/Footer/Footer.jsx
+++ react/src/components/Footer/Footer.jsx
@@ -1,16 +1,12 @@
-import './Footer.css';
+import "./Footer.css";
 
 export default function Footer() {
-    return(
-
-        <footer className="container d-flex justify-content-between align-items-center mt-5 pt-5 pb-3 border-top border-secondary border-opacity-25">
-        <div className="pagination-dots">
-          <span className="dot active"></span>
-          <span className="dot"></span>
-          <span className="dot"></span>
-        </div>
-        <p className="text-gray-secondary mb-0 fs-6">Página 1 de 3</p>
-      </footer>
-
-    );
+  return (
+   
+   <footer className="text-center container d-flex justify-content-between align-items-center mt-5 pt-5 pb-3 border-top border-secondary border-opacity-25">
+      <div className="container">
+        <p className="mb-0 footer-text">&copy; TODOS OS DIREITOS RESERVADOS.</p>
+      </div>
+    </footer>
+  );
 }
